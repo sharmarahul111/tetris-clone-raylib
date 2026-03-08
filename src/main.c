fb6a78ceb0b0a7e021cb2526dbd1883515ac211a
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-03-08 19:39:40 +0545

    structure typedefs and formatting

--- src/main.c
+++ src/main.c
@@ -1,26 +1,34 @@
-#include <stdio.h>
 #include <raylib.h>
+#include <stdio.h>
 #define GRID_COLS 10
 #define GRID_ROWS 20
 #define CELL_SIZE 30
-#define BOARD_WIDTH  (GRID_COLS * CELL_SIZE)
+#define BOARD_WIDTH (GRID_COLS * CELL_SIZE)
 #define BOARD_HEIGHT (GRID_ROWS * CELL_SIZE)
 #define SIDEBAR_WIDTH 200
-#define WINDOW_WIDTH  (BOARD_WIDTH + SIDEBAR_WIDTH)
+#define WINDOW_WIDTH (BOARD_WIDTH + SIDEBAR_WIDTH)
 #define WINDOW_HEIGHT BOARD_HEIGHT
+
+typedef struct {
+	int width;
+	int height;
+	int data[5][5];
+	Color color;
+} Block;
+typedef Color Cell;
 void game_loop(void);
-int main(){
-	int GRID[GRID_ROWS][GRID_COLS];
+int main() {
+	Cell GRID[GRID_ROWS][GRID_COLS];
 	printf("Running...\n");
 	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Tetris Clone");
 	SetTargetFPS(60);
-	while(!WindowShouldClose()){
+	while (!WindowShouldClose()) {
 		game_loop();
 	}
 	CloseWindow();
 	return 0;
 }
-void game_loop(){
+void game_loop() {
 	BeginDrawing();
 	DrawRectangle(20, 20, CELL_SIZE, CELL_SIZE, BLUE);
 	EndDrawing();
